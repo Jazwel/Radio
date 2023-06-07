ea788f6e046448f7a65c48baef24749471d0c0bd
DS <mc_sd@bk.ru>
2023-06-07 20:15:10 -0300

    Reformat code

--- src/main/java/ru/netology/stats/Radio.java
+++ src/main/java/ru/netology/stats/Radio.java
@@ -4,37 +4,38 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
 
-            public int getCurrentStation () {
-                return currentStation;
-            }
+    public int getCurrentStation() {
+        return currentStation;
+    }
 
-            public void setCurrentStation (int currentStation){
-                if (currentStation < 0) {
-                    return;
-                }
-                if (currentStation > 9) {
-                    return;
-                }
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
 
-                this.currentStation = currentStation;
-            }
+        this.currentStation = currentStation;
+    }
 
-            public void next () {
-                if (currentStation != 9) {
-                    currentStation++;
-                } else {
-                    currentStation = 0;
-                }
-            }
+    public void next() {
+        if (currentStation != 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
 
-            public void prev () {
-                if (currentStation != 0) {
-                    currentStation--;
-                } else {
-                    currentStation = 0;
-                }
-            }
-    public int getCurrentVolume(){
+    public void prev() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public int getCurrentVolume() {
         return currentVolume;
     }
 
@@ -42,25 +43,28 @@ public class Radio {
         if (currentVolume < 0) {
             return;
         }
-        if (currentVolume >  100) {
+        if (currentVolume > 100) {
             return;
 
         }
         this.currentVolume = currentVolume;
     }
-            public void increaseVolume() {
-                if (currentVolume < 100) {
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
             currentVolume = currentVolume + 1;
         }
     }
-    public void prevVolume () {
+
+    public void prevVolume() {
         if (currentVolume != 0) {
             currentVolume--;
         } else {
             currentVolume = 0;
         }
     }
-    public void nextVolume () {
+
+    public void nextVolume() {
         if (currentVolume != 100) {
             currentVolume++;
         } else {
@@ -69,7 +73,7 @@ public class Radio {
     }
 
 
-        }
+}
 
 
 
